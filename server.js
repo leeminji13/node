65bdbeffac5b0fc9938a4da3c5004a01fdc4a7f8
leeminji <minjis2s2s2@gmail.com>
2022-03-16 17:18:08 +0900

    sql연결

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require('express');
 // var portlink = require('./routes/router')
 var app = express(); //express module instuns app
 var time = new Date(); //기본적으로 컴퓨터가 쓰고 있는 매체(절대적으로 한개밖에 없음)
+var mysql = require('mysql')
+var dbconfig = require('./db/config.js')
+var connection = mysql.createConnection(dbconfig)
 
 var postsend = require('./api/postsend')//application protocal interface
 var getsend = require('./api/getsend')
@@ -10,6 +13,15 @@ var getsend = require('./api/getsend')
 app.set('port',8080)
 app.get('/',(req,res)=>{
     res.send('/루트요청 있었음')
+})
+
+app.get('/introduce',(req,res)=>{
+    connection.query(
+        'SELECT * FROM introduce_react_interview.Users',
+        (error,result)=>{
+            if(error)throw error;
+            res.send(result)
+        })
 })
 
 //리액트에서 비동기로 정보요청할 주소 세팅
